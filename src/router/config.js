4e77294966b5328c6962d09a6f02e0fb1fb3b2a8
lu.wang <158322321@qq.com>
2017-12-01 10:32:23 +0800

    1

---
+++ src/router/config.js
@@ -0,0 +1,7 @@
+import home from '../views/home/index.vue'
+
+const path = [
+    {path:'/',component:home}
+]
+
+export default path
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import home from '../views/home/index.vue'
+import config from './config.js'
 
 Vue.use(VueRouter)
 
 const path = '../../views/'
 const router = new VueRouter({
   mode: 'history',
-  routes :[
-	{ path: '/', component:home }
-  ]
+  routes :config
 })
 
 export default router
